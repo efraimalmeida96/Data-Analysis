d36bda8a567c9b302e9b503a92906cafcd45be31
Efraim <efraimnba@gmail.com>
2026-03-06 13:10:55 -0300

    part2 - ex1-6

--- nba_games.py
+++ nba_games.py
@@ -1,11 +1,13 @@
 # %%
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+#%%
 df = pd.read_csv('./files/game.csv')
 df
-
 # %%
-df['win_home'] = df["wl_home"] == 'W'
+
 # %%
 ### 1: victory per season (home team)
 # size shows how much; value_counts shows the frequency
@@ -17,7 +19,7 @@ df[df['win_home']].groupby('team_name_home').size()
 df2 = df[df['game_date'] >= '2021-10-19 00:00:00'].copy()
 df2
 # %%
-df2['win_home'] = df2["wl_home"] == 'W'
+df2['win_home'] = df2["wl_home"] == 'W' # create a new column 'win_home' that is True if the home team won
 df2['win_home']
 #  %%
 df2[df2['win_home']].groupby(['team_name_home']).size()
@@ -59,7 +61,7 @@ points_hm.plot.line(rot=90)
 # %%
 
 ### 7: relations betweeen turnovers and loses
-import matplotlib.pyplot as plt
+
 # tov_home = df2.groupby('wl_home')[['tov_home']].mean()
 # mean_to_lose = df2[df['wl_home'] == 'L']['tov_home'].mean()
 # mean_to_win = df2[df['wl_home'] == 'W']['tov_home'].mean()
@@ -122,3 +124,83 @@ df2['win_rate_home'] = df2['wl_home'] == 'W'
 win_rate_home = df2.groupby('team_name_home')['win_rate_home'].mean().sort_values(ascending=False).head(5)
 
 win_rate_home
+
+#%%
+
+# -=-=-=-=-=-=-==-= PART II =-=-=-=-=-=-==-=-
+
+### 1 Game with more points combine 
+df2['sum_points'] = df2['pts_home'] + df2['pts_away']
+
+df2[['sum_points', 'matchup_home']].sort_values(by='sum_points', ascending=False).head(1)
+
+#%%
+# too slow, scan all the dataframe but works and it's a clean code
+# df2[['game_id', 'game_date', 'sum_points', 'matchup_home']].sort_values(by='sum_points', ascending=False).head(1)
+
+df2['sum_points'] = df2['pts_home'] + df2['pts_away']
+
+idx = df2['sum_points'].idxmax() # find the game with most point by the index
+
+result = df2.loc[idx, ['game_id', 'game_date', 'matchup_home', 'sum_points']]
+
+result
+
+# %%
+### 2 games with overtime
+
+# The 'min' column represents the total minutes played by the team.
+# Standard game = 240 (48 mins * 5 players). > 240 implies overtime.
+df2[df2['min'] > 240][['game_date', 'matchup_home', 'min']]
+
+#%%
+# df2.groupby('season_id').apply(lambda x: (x['min'] > 240).sum())
+games_ot_per_season = df2[df2['min']> 240].groupby('season_id').size() 
+
+games_ot_per_season
+
+# %%
+### 3 how many games had on each season 
+games_per_season = df2.groupby('season_id').size()
+
+games_per_season.plot.line() # pandas has a built-in plotting function
+
+# %%
+# with matplotlib
+
+plt.plot(games_per_season.index, games_per_season.values) # x data, y data
+plt.show
+
+#%%
+# with seaborn
+
+
+sns.lineplot(x=games_per_season.index, y=games_per_season.values)
+
+#%%
+### 4 home win percentage by season
+
+# df2[['season_id', 'game_date', 'game_id', 'matchup_away', 'matchup_home']]
+
+home_win_percentage = df2.groupby('season_id')['game_id'].size()
+# home_win_percentage = df2.groupby('season_id')['game_id'].count()
+
+home_win_percentage
+#%%
+# counted all games by filtering the win home games
+total_win_home_team = df2[df2['wl_home'] == 'W'].groupby('season_id').size()
+total_win_home_team
+
+#%%
+
+hm_win_perctge = (total_win_home_team / games_per_season) * 100
+hm_win_perctge
+
+#%% 
+### 5 Average points in wins vs losses (home team)
+
+avg_points_w_vs_l = df2.groupby(['season_id', 'wl_home'])['pts_home'].mean().unstack('wl_home') # : Pivots the 'wl_home' (W/L) into columns, giving a DataFrame
+avg_points_w_vs_l
+
+#%%
+### 6 
